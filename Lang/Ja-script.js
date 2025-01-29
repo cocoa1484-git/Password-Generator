59d3f64dbd9319b85c74752c1984e4fe7a0eafc9
cocoa1484 <seddle.PAL@gmail.com>
2025-01-29 10:33:22 +0900

    Update Ja-script.js

--- Lang/Ja-script.js
+++ Lang/Ja-script.js
@@ -30,5 +30,5 @@ function copyToClipboard() {
     passwordField.select();
     passwordField.setSelectionRange(0, 99999); // For mobile devices
     navigator.clipboard.writeText(passwordField.value);
-    alert('パスワードをコピーしました。: ' + passwordField.value);
+    alert('%E3%83%91%E3%82%B9%E3%83%AF%E3%83%BC%E3%83%89%E3%82%92%E3%82%B3%E3%83%94%E3%83%BC%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F%E3%80%82: ' + passwordField.value);
 }
